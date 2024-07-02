c2c1f2ad4340c7d08f9740c0120ebea1e5b03ae3
delsyke <wasykepaul@gmail.com>
2024-07-02 22:28:57 +0300

    now uses crew codes instead of names

--- read_crew.py
+++ read_crew.py
@@ -11,18 +11,15 @@ with open('names.json','r') as f:
     all_crew=json.load(f)
 
 
-def edit_create_flight(name1, name2, all_crew, callsign):
+def edit_create_flight(pic_code, sic_code, all_crew, callsign):
     for crew in all_crew:
-        PIC = crew['fullname'].lower()
-        if PIC.startswith(name1.lower()) or PIC.endswith(name1.lower()):
+        if crew["crewCode"] == pic_code:
             pic_user = crew['username']
             flight_details['flight']['crew'][0]['crewId'] = pic_user
             break
 
-
     for crew in all_crew:
-        SIC = crew['fullname'].lower()
-        if SIC.startswith(name2.lower()) or SIC.endswith(name2.lower()):
+        if crew["crewCode"] == sic_code:
             sic_user = crew['username']
             flight_details['flight']['crew'][1]['crewId'] = sic_user
             break
